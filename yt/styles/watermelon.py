c35c89bb5e4b44c66884e111b7909c5e4b4f68b8
HEran-ite <137596848+HEran-ite@users.noreply.github.com>
2023-10-19 22:18:14 +0300

    Delete yt directory

--- yt/styles/watermelon.py
+++
@@ -1,8 +0,0 @@
-def watermelon():
-    w=input('input:' )
-    for w in range(1,100):
-        if w==2:return False
-        elif (w-2)%2==0:return True
-        else:return False
-    return watermelon(w-2)
-print(watermelon())   
